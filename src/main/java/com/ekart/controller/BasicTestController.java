baa83b7f10f47ae5ed21b604ed4c18137e208cca
user <user@CHRISTIAN-GRAY>
2020-02-27 16:01:30 +0530

    Adding a new home page and testing carrying information

--- src/main/java/com/ekart/controller/BasicTestController.java
+++ src/main/java/com/ekart/controller/BasicTestController.java
@@ -3,6 +3,7 @@ package com.ekart.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class BasicTestController 
@@ -23,5 +24,13 @@ public class BasicTestController
 	public String greeting2()
 	{
 		return"founder";
+	}
+	
+	@RequestMapping("/carryinfo")
+	public ModelAndView takinginfo()
+	{
+		ModelAndView modelAndView=new ModelAndView("displayinfo");
+		modelAndView.addObject("info", 3000);
+		return modelAndView;
 	}
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -3,5 +3,6 @@
 <h2>Hello World!</h2>
 <a href="hello">greetings</a>
 <a href="details">founder</a>
+<a href="carryinfo">information</a>
 </body>
 </html>
